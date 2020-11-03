3604046a1185d2818c45b65e7e98468b28758fba
MariKush <43176370+MariKush@users.noreply.github.com>
2020-11-03 23:24:45 +0200

    add more analyze and some rename

--- lab2/static_analyzer.py
+++ lab2/static_analyzer.py
@@ -34,18 +34,28 @@ class StaticAnalyzer:
     def __init__(self, files):
         self.files = files
 
-    def validate_camel_case_first_up(self, token):
+    # MyPascal
+    def validate_pascal_case(self, token):
         pass
 
     def validate_interface(self, token):
         pass
 
+    def validate_camel_case(self, token):
+        pass
+
     def validate_names(self, file):
         stack_influential_tokens = []
+        was_const = False
         for index in range(len(file.all_tokens)):
             current_token = file.all_tokens[index]
             if current_token.token_value in ['class', 'enum', 'namespace', 'interface', '{']:
                 stack_influential_tokens.append(current_token)
+
+            if current_token.token_value == 'const':
+                was_const = True
+            elif current_token.token_value in ['{', ';', '}', '=']:
+                was_const = False
 
             if current_token.token_value == '}':
                 stack_influential_tokens.pop()
@@ -55,16 +65,29 @@ class StaticAnalyzer:
 
             if current_token.token_type == TokenType.NUMBER_OR_IDENTIFIERS:
                 previous_significant_token = file.all_tokens[find_previous_significant_token_index(file, index)]
+                next_significant_token = file.all_tokens[find_next_significant_token_index(file, index)]
                 if len(stack_influential_tokens) > 0 and stack_influential_tokens[-1].token_value in \
                         ['namespace', 'class', 'enum']:
-                    self.validate_camel_case_first_up(current_token)
+                    self.validate_pascal_case(current_token)
                 elif len(stack_influential_tokens) > 0 and stack_influential_tokens[-1].token_value == 'interface':
                     self.validate_interface(current_token)
+                elif len(stack_influential_tokens) > 1 and stack_influential_tokens[-1].token_value == '{' and \
+                        stack_influential_tokens[-2] == 'enum':
+                    self.validate_pascal_case(current_token)
                 elif len(stack_influential_tokens) > 1 and stack_influential_tokens[-1].token_value == '{' and \
                         stack_influential_tokens[-2] in ['class', 'interface'] and \
                         (previous_significant_token.token_value in ['>', ']'] or
                             previous_significant_token.token_type == TokenType.NUMBER_OR_IDENTIFIERS or
                             previous_significant_token.token_value in keyword_type_value):
+                    if was_const:
+                        self.validate_pascal_case(current_token)
+                    elif next_significant_token.token_value in ['(', '{']:
+                        self.validate_pascal_case(current_token)
+                    elif next_significant_token.token_value in [';', '=']:
+                        self.validate_camel_case(current_token)
+
+
+
                     pass
                 # TODO for field method property
 
